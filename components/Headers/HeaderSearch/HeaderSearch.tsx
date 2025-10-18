99951b71bac90ccedb570e90e107591ea88d86ec
Timothe Duquesne <timothe.duquesne@gmail.com>
2025-10-18 15:26:14 +0200

    feat: added searchbar component

--- components/Headers/HeaderSearch/HeaderSearch.tsx
+++ components/Headers/HeaderSearch/HeaderSearch.tsx
@@ -1,10 +1,19 @@
 import styles from "./HeaderSearch.module.css";
 import Logo from "@/components/Logo/Logo";
+import SearchBar from "@/components/SearchBar/SearchBar";
 
 export default function HeaderSearch() {
   return (
     <header className={styles.container}>
       <Logo />
+      <div className={styles.heroContainer}>
+        <p className={styles.hero}>
+          {"Découvrez nos recettes".toUpperCase()}
+          <br />
+          {"du quotidien, simples et délicieuses".toUpperCase()}
+        </p>
+        <SearchBar />
+      </div>
     </header>
   );
 }
---
+++ components/SearchBar/SearchBar.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useState } from "react";
+import styles from "./SearchBar.module.css";
+
+export default function SearchBar() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const isCrossVisible = searchQuery.length > 0;
+
+  return (
+    <div className={styles.searchBarContainer}>
+      <input
+        type="text"
+        name=""
+        id=""
+        className={styles.searchBar}
+        placeholder="Rechercher une recette, un ingrédient,..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+
+      <div className={styles.buttonsContainer}>
+        {isCrossVisible && (
+          <button
+            type="button"
+            className={styles.cross}
+            onClick={() => setSearchQuery("")}
+            aria-label="Effacer la recherche"
+          >
+            <svg
+              width="17"
+              height="17"
+              viewBox="0 0 17 17"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M15 15L8.5 8.5M8.5 8.5L2 2M8.5 8.5L15 2M8.5 8.5L2 15"
+                stroke="#7A7A7A"
+                strokeWidth="2.16667"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </button>
+        )}
+
+        <button className={styles.loopCTA}>
+          <svg
+            width="28"
+            height="28"
+            viewBox="0 0 28 28"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="10" cy="10" r="9.5" stroke="white" />
+            <line
+              x1="18.3536"
+              y1="18.6464"
+              x2="27.3536"
+              y2="27.6464"
+              stroke="white"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+}
