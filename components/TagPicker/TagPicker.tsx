889ff2e81d5a55479b8ca4af166c289155611aad
Timothe Duquesne <timothe.duquesne@gmail.com>
2025-10-22 20:39:18 +0200

    feat: added tag under the tag picker

--- components/TagPicker/TagPicker.tsx
+++ components/TagPicker/TagPicker.tsx
@@ -29,71 +29,105 @@ export default function TagPicker({
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className={clsx(styles.container, { [styles.openContainer]: isOpen })}>
-      <div className={styles.alwaysDisplayedPart}>
-        <p className={styles.tagPickerName}>{tagPickerName}</p>
-        <button className={styles.arrowButton}>
-          <Image
-            src={arrow}
-            alt="Open or close the tag dropdown picker"
-            height={6}
-            width={13}
-            onClick={() => setIsOpen(!isOpen)}
-            className={clsx(styles.image, { [styles.imageTurn]: isOpen })}
-          />
-        </button>
-      </div>
-
-      <div className={styles.collapsibleContainer}>
-        <div className={styles.searchContainer}>
-          <TagPickerSearchBar
-            searchText={searchText}
-            setSearchText={setSearchText}
-          />
+    <div>
+      <div
+        className={clsx(styles.container, { [styles.openContainer]: isOpen })}
+      >
+        <div className={styles.alwaysDisplayedPart}>
+          <p className={styles.tagPickerName}>{tagPickerName}</p>
+          <button className={styles.arrowButton}>
+            <Image
+              src={arrow}
+              alt="Open or close the tag dropdown picker"
+              height={6}
+              width={13}
+              onClick={() => setIsOpen(!isOpen)}
+              className={clsx(styles.image, { [styles.imageTurn]: isOpen })}
+            />
+          </button>
         </div>
 
-        {selectedTags.length > 0 && (
-          <ul className={styles.tagFilteredList}>
-            {selectedTags.map((tag, index) => (
-              <li key={index} className={styles.tagFiltered}>
-                <p>{tag}</p>
-                <button
-                  onClick={() => removeTag(tag)}
-                  className={styles.filteredTagButton}
-                >
-                  <svg
-                    width="17"
-                    height="17"
-                    viewBox="0 0 17 17"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
+        <div className={styles.collapsibleContainer}>
+          <div className={styles.searchContainer}>
+            <TagPickerSearchBar
+              searchText={searchText}
+              setSearchText={setSearchText}
+            />
+          </div>
+
+          {selectedTags.length > 0 && (
+            <ul className={styles.tagFilteredList}>
+              {selectedTags.map((tag, index) => (
+                <li key={index} className={styles.tagFiltered}>
+                  <p>{tag}</p>
+                  <button
+                    onClick={() => removeTag(tag)}
+                    className={styles.filteredTagButton}
                   >
-                    <path
-                      d="M15 15L8.5 8.5M8.5 8.5L2 2M8.5 8.5L15 2M8.5 8.5L2 15"
-                      stroke="#7A7A7A"
-                      strokeWidth="2.16667"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </button>
+                    <svg
+                      width="17"
+                      height="17"
+                      viewBox="0 0 17 17"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M15 15L8.5 8.5M8.5 8.5L2 2M8.5 8.5L15 2M8.5 8.5L2 15"
+                        stroke="#7A7A7A"
+                        strokeWidth="2.16667"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+
+          <ul className={styles.tagList}>
+            {[...availableTags].map((tag, index) => (
+              <li
+                key={index}
+                className={styles.listElement}
+                onClick={() => addTag(tag)}
+              >
+                {tag}
               </li>
             ))}
           </ul>
-        )}
-
-        <ul className={styles.tagList}>
-          {[...availableTags].map((tag, index) => (
-            <li
-              key={index}
-              className={styles.listElement}
-              onClick={() => addTag(tag)}
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
+        </div>
       </div>
+
+      {selectedTags.length > 0 && (
+        <div className={styles.tagSelectionContainer}>
+          {selectedTags.map((tag) => (
+            <div key={tag} className={styles.tagSelected}>
+              <p>{tag}</p>
+              <button
+                className={styles.removeSelectedTag}
+                onClick={() => removeTag(tag)}
+              >
+                <svg
+                  width="17"
+                  height="17"
+                  viewBox="0 0 17 17"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M15 15L8.5 8.5M8.5 8.5L2 2M8.5 8.5L15 2M8.5 8.5L2 15"
+                    stroke="#7A7A7A"
+                    strokeWidth="2.16667"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
